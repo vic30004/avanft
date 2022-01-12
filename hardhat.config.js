8a9ab15560f654e6fd78791e468a737a9f2ab0e9
Victor <victor_993@msn.com>
2022-01-11 19:00:53 -0800

    configured hardhat and setup minting functionality (#1)

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,38 @@
 require("@nomiclabs/hardhat-waffle");
+const fs = require("fs");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
+const keyData = fs.readFileSync("./p-key.txt", {
+  encoding: "utf8",
+  flag: "r",
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+const projectKeyData = fs.readFileSync("projectKey.txt", {
+  encoding: "utf-8",
+  flag: "r",
+});
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337, //config standard
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectKeyData}`,
+      accounts: [keyData],
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${projectKeyData}`,
+      accounts: [keyData],
+    },
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
 };
