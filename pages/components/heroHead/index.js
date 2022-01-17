368b4af0d8a3d01b01ee5b2db3f1e5f2cb83ddf4
victor aa <victor_993@msn.com>
2022-01-16 18:41:00 -0800

    created hero head

---
+++ pages/components/heroHead/index.js
@@ -0,0 +1,9 @@
+import ContentCotainer from "./ContentCotainer";
+
+export default function HeroHead() {
+  return (
+    <div className='flex relative'>
+      <ContentCotainer />
+    </div>
+  );
+}
---
+++ pages/components/ui/button/index.js
@@ -0,0 +1,7 @@
+export default function button({ text }) {
+  return (
+    <>
+      <button className=' bg-purple-dark title-font px-7 py-3 rounded-lg text-sm my-4'>{text}</button>
+    </>
+  );
+}
