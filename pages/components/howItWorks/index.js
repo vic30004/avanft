f1c9085fa03ead933e314430e5b930071ffac469
victor aa <victor_993@msn.com>
2022-01-17 13:12:29 -0800

    updated style

--- pages/components/howItWorks/index.js
+++ pages/components/howItWorks/index.js
@@ -3,8 +3,10 @@ import InfoBlocks from "./InfoBlocks";
 export default function HowItWorks() {
   return (
     <section className='md:pl-123 pt-10 bg-blue-light'>
-      <h1 className='title-font text-2xl text-center md:text-left'>How It Works</h1>
-      <div className='flex flex-wrap py-8 md:w-2/3 items-center md:justify-start justify-center'>
+      <h1 className='title-font text-2xl text-center md:text-left'>
+        How It Works
+      </h1>
+      <div className='flex flex-wrap md:flex-nowrap py-8 md:w-2/3 items-center md:justify-start justify-center'>
         <InfoBlocks
           svg={
             "https://res.cloudinary.com/dawyijhjw/image/upload/v1642453178/AVANFT/earth-alt-svgrepo-com_1_aangsr.svg"
